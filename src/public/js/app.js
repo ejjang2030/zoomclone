ac9d2ad8b83e892cc34cc8da9c33351808ac680f
Eunjae Jang <ejjang2030@gmail.com>
2022-08-12 13:45:16 +0900

    feat : Initialization

---
+++ src/public/js/app.js
@@ -0,0 +1,15 @@
+const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function handleRoomSubmit(event) {
+    event.preventDefault();
+    const input = form.querySelector("input");
+    socket.emit("enter_room", input.value, () => {
+        console.log("server is done!");
+    })
+    input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
