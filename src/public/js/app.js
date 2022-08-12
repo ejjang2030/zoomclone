3b0e2e7ab57d94e47cf7ac1bf72867279ac24d22
Eunjae Jang <ejjang2030@gmail.com>
2022-08-12 15:01:26 +0900

    feat : users count

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -54,14 +54,31 @@ function handleRoomSubmit(event) {
 
 form.addEventListener("submit", handleRoomSubmit);
 
-socket.on("welcome", (userNickname) => {
+socket.on("welcome", (userNickname, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName} (${newCount})`;
     addMessage(`${userNickname} arrived!`);
 })
 
-socket.on("bye", (userNickname) => {
+socket.on("bye", (userNickname, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName} (${newCount})`;
     addMessage(`${userNickname} left ㅠ ㅠ`);
 })
 
 socket.on("new_message", (msg) => {
     addMessage(msg);
+})
+
+socket.on("room_change", (rooms) => {
+    const roomList = welcome.querySelector("ul");
+    roomList.innerHTML = "";
+    if (rooms.length === 0) {
+        return;
+    }
+    rooms.forEach((room) => {
+        const li = document.createElement("li");
+        li.innerText = room;
+        roomList.append(li);
+    })
 })
